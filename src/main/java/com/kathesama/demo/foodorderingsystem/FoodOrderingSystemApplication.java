5cf017f5a6608f28d1357978909a391afbd10688
kathemica <kathemica@gmail.com>
2024-04-01 14:53:16 -0300

    first commit

---
+++ src/main/java/com/kathesama/demo/foodorderingsystem/FoodOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kathesama.demo.foodorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderingSystemApplication.class, args);
+    }
+
+}
